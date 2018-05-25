d6f20793e2b027c38d8c059bbd7fa8cc7ca35a2d
meg <dearmegwhite@gmail.com>
2018-05-25 16:31:05 +1000

    yess

--- mfcc_version.py
+++ mfcc_version.py
@@ -10,34 +10,18 @@ from python_speech_features import logfbank
 import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
 import librosa
+import librosa.display
 
-num_features = 13
 
-def wav_to_mfcc(file):
-    # https://github.com/jameslyons/python_speech_features/blob/master/example.py
-    fs, signal = wav.read(file)
-    mfcc_feat = mfcc(signal,fs)
-    #d_mfcc_feat = delta(mfcc_feat, 2) # change
-    #fbank_feat = logfbank(sig,rate) # change
+n_mfcc = 13
+n_mels = 40
+n_fft = 512
+win_length = 400 # 0.025*16000
+hop_length = 160 # 0.010 * 16000
+window = 'hamming'
+fmin = 20
+fmax = 4000
 
-    return mfcc_feat
-
-thing1 = wav_to_mfcc('test.wav')
-thing2 = wav_to_mfcc('test-y.wav')
-plt.plot(thing1)
-plt.show()
-print(thing1.shape)
-print(thing2.shape)
-
-temp =  thing1[ : , np.newaxis , :] # adds 1 in the middle, not sure why i'd want that tbh
-temp2=  thing2[ : , np.newaxis , :]
-
-#print(temp2.shape)
-input_var = Variable(torch.Tensor(temp))
-input_var2 = Variable(torch.Tensor(temp2))
-
-print(input_var.shape)
-print(input_var2.shape)
 
 
 ## USING LIBROSA
@@ -47,30 +31,11 @@ MELBANK = 80
 y, sr = librosa.load('test.wav')
 # melspec = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=100, fmax = 8000, n_mels=QUANT)
 melspec = librosa.feature.melspectrogram(y=y, sr=sr)
-print(melspec)
-
-print(melspec.shape) #128, 265
-
 melspec = melspec[:,:-1]
-
-print(melspec)
-
-print(melspec.shape)#128, 264
-
-plt.plot(melspec)
-plt.show()
-
-
-s = np.abs(librosa.core.stft(y=y, n_fft=800, hop_length=MELWINDOW, window='hann', center=True))
-voice_input_oh = librosa.feature.melspectrogram(S=s, n_mels=MELBANK, fmax=7600, fmin=125, power=2, n_fft = 800, hop_length=MELWINDOW)
-voice_input_oh = librosa.core.amplitude_to_db(S=voice_input_oh, ref=1.0, amin=5e-4, top_db=80.0) #logamplitude
-librosa.display.specshow(voice_input_oh)
-print(voice_input_oh)
-print(voice_input_oh.shape) # 80, 678
-
-plt.plot(voice_input_oh)
-plt.show()
-
+librosa.display.specshow(librosa.power_to_db(melspec, ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Mel spectrogram')
+plt.tight_layout()
 
 
 # class AudioMagicNet(nn.Module):
@@ -106,3 +71,70 @@ plt.show()
 #         h = self.final2(h)
 #         print(h.shape)
 #         return h
+
+###### CODE CEMETARY #######
+
+
+# def wav_to_mfcc(file):
+#     # https://github.com/jameslyons/python_speech_features/blob/master/example.py
+#     # signal, samplerate=16000, winlen=0.025, winstep=0.01, numcep=13, nfilt=26, nfft=512, lowfreq=0, highfreq=None, preemph=0.97, ceplifter=22, appendEnergy=True, winfunc=<function <lambda>>
+#     fs, signal = wav.read(file)
+#     mfcc_feat = mfcc(signal,fs, numcep=80)
+#     #d_mfcc_feat = delta(mfcc_feat, 2) # change
+#     fbank_feat = logfbank(signal,fs, nfilt=80) # change)
+
+#     # return mfcc_feat
+#     return fbank_feat
+
+# thing1 = wav_to_mfcc('test.wav')
+# thing2 = wav_to_mfcc('test-y.wav')
+# plt.plot(thing1)
+# plt.show()
+# print(thing1.shape)
+# print(thing2.shape)
+
+# temp =  thing1[ : , np.newaxis , :] # adds 1 in the middle, not sure why i'd want that tbh
+# temp2=  thing2[ : , np.newaxis , :]
+
+# #print(temp2.shape)
+# input_var = Variable(torch.Tensor(temp))
+# input_var2 = Variable(torch.Tensor(temp2))
+
+# print(input_var.shape)
+# print(input_var2.shape)
+
+# y, sr = librosa.load('test.wav')
+# # melspec = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=100, fmax = 8000, n_mels=QUANT)
+# melspec = librosa.feature.melspectrogram(y=y, sr=sr)
+#
+
+# print(melspec)
+
+# print(melspec.shape) #128, 265
+
+# melspec = melspec[:,:-1]
+
+# print(melspec)
+
+# print(melspec.shape)#128, 264
+
+# plt.plot(melspec)
+# plt.show()
+
+
+#n_fff is window size
+
+# s = np.abs(librosa.core.stft(y=y, n_fft=800, hop_length=MELWINDOW, window='hann', center=True)) # pre-computed power spec
+# voice_input_oh = librosa.feature.melspectrogram(S=s, n_mels=MELBANK, fmax=7600, fmin=125, power=2, n_fft = 800, hop_length=MELWINDOW) # passed to melfilters
+# voice_input_oh = librosa.core.amplitude_to_db(S=voice_input_oh, ref=1.0, amin=5e-4, top_db=80.0) #logamplitude
+# librosa.display.specshow(voice_input_oh, y_axis='log', x_axis='time')
+# plt.title('Power spectrogram')
+# plt.colorbar(format='%+2.0f dB')
+# plt.tight_layout()
+
+
+# print(voice_input_oh)
+# print(voice_input_oh.shape) # 80 coefficients, 678 frames
+
+# plt.plot(voice_input_oh)
+# plt.show()
