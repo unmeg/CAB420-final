cf4f8460a82444cff12622f7e681a265a0bdfe80
Lindsay <lindsayawatt@gmail.com>
2018-05-26 19:00:24 +1000

    things r werkin

--- data/generate_pesq_dataset.py
+++ data/generate_pesq_dataset.py
@@ -10,11 +10,21 @@ from scipy import signal
 from scipy.io import wavfile
 from scipy import interpolate
 import librosa
+import matplotlib.pyplot as plt
 
 
+num_classes = 50
 pesq_sr = 16000
 input_hdf5 = 'train_vctk_patches.hdf5'
 output_hdf5 = 'train_pesq.hdf5'
+
+
+def encode_score(score):
+    i = score * (num_classes / 5)
+    z = np.zeros(num_classes)
+    if i != 0:
+        z[int(i)] = 1
+    return np.expand_dims(z, axis=0)
 
 
 def upsample(x_lr, d, r):
@@ -33,70 +43,80 @@ def upsample(x_lr, d, r):
 
 def resample(x, original_sr, new_sr, filename, noise):
 
-    x = librosa.resample(x, original_sr, new_sr, res_type='kaiser_fast')
+    r = original_sr // new_sr
+    resampled = x.copy()[::r] # librosa.resample(x.copy(), original_sr, new_sr, res_type='kaiser_fast')
+    resampled = upsample(resampled, 0, r)
+
 
     # generate cracking noise thing
     if noise:
-        r = np.random.rand(x.shape[0]) * 0.5
+        r = np.random.rand(resampled.shape[0]) * 0.5
         b = r < 0.001
         r[~b] = 0
         r[b] = np.random.rand(1) * 0.05
-        x += r
+        resampled += r
 
     filename_out = os.path.join('temp', str(sr) + '_' + str(noise) + '_' + filename)
-    sf.write(filename_out, x, pesq_sr, 'PCM_16')
-    return filename_out
+    sf.write(filename_out, resampled, pesq_sr, 'PCM_16')
+    return (filename_out, resampled)
 
 
-h5_file = h5py.File('train_patches.hdf5', 'r')
+h5_file = h5py.File(input_hdf5, 'r')
 hr_dataset = h5_file['hr']
 
 scores = []
 patches = []
 
-indexes = random.shuffle(range(hr_dataset.shape[0]))
-for i in indexes:
+indexes = list(range(hr_dataset.shape[0]))
+random.shuffle(indexes)
+for hi in indexes:
 
     # grab a hr patch from the hdf5
-    x = hr_dataset[i, 0, :]
-    hr_filename = '{:d}_hr.wav'.format(i)
+    x = hr_dataset[hi, 0, :]
+    hr_filename = '{:d}_hr.wav'.format(hi)
 
     # store filenames in here of the downsampled and noisy (degraded) versions of this patch
-    degraded_files = []
+    these_scores = []
+    degraded_x = []
 
     # take the hr reference and downsample it to the following sr's, make one with noise and one without for each
     sampling_rates = [16000, 8000, 4000, 2000, 1000]
     for sr in sampling_rates:
-        degraded_files.append('temp', resample(x, pesq_sr, sr, hr_filename, False))
-        degraded_files.append('temp', resample(x, pesq_sr, sr, hr_filename, True))
+        degraded_x.append(resample(x, 16000, sr, hr_filename, False))
+        degraded_x.append(resample(x, 16000, sr, hr_filename, True))
 
-    for i in range(len(degraded_files)):
+    # get the score of every degraded version
+    for i in range(len(degraded_x)):
         try:
-            score = get_pesq(degraded_files[0], degraded_files[i])
-            scores.append(score)
-            patches.append(np.append(x, y))
-        except:
+            score = get_pesq(degraded_x[0][0], degraded_x[i][0])
+            these_scores.append(score)
+            patches.append(np.append(degraded_x[0][0], degraded_x[-1][0]))
+        except Exception as e:
             pass
         finally:
             if i > 0:
-                os.remove(degraded_files[i])
+                pass
+                os.remove(degraded_x[i][0])
 
-    os.remove(degraded_files[0])
+    os.remove(degraded_x[0][0])
 
-    for i in range(len(degraded_files)):
-        print('{:.3f}\t{:s}'.format(scores[i], os.path.basename(degraded_files[i])))
+    print('\n')
+    for di in range(len(these_scores)):
+        print('{:.3f}\t{:s}'.format(these_scores[di], os.path.basename(degraded_x[di][0])))
+
+    scores += these_scores
 
 patches = np.array(patches)
 scores = np.array(scores)
 
-with h5py.File('pesq_test.hdf5', 'w') as f:
+with h5py.File(output_hdf5, 'w') as f:
     x = f.create_dataset("x", patches.shape, dtype=np.float32)
     y = f.create_dataset("y", scores.shape, dtype=np.float32)
 
     x[...] = patches
     y[...] = scores
 
-h5_file = h5py.File('pesq_test.hdf5', 'r')
+h5_file = h5py.File(output_hdf5, 'r')
 x = h5_file['x']
 y = h5_file['y']
 
