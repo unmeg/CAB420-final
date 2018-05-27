d336b03cd4e7885e8be26af3592092acfe27e945
Lindsay <lindsayawatt@gmail.com>
2018-05-27 11:11:12 +1000

    bits

--- data/generate_pesq_dataset.py
+++ data/generate_pesq_dataset.py
@@ -11,16 +11,25 @@ from scipy.io import wavfile
 from scipy import interpolate
 import librosa
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 
 
 num_classes = 50
 pesq_sr = 16000
-input_hdf5 = 'train_patches.hdf5'
+input_hdf5 = 'train_vctk_patches.hdf5'
 output_hdf5 = 'train_pesq.hdf5'
 
 
+def score2index(score):
+    return round(score, 1) * (num_classes / 5)
+
+
+def index2score(index):
+    return (i / (num_classes / 5))
+
+
 def encode_score(score):
-    i = round(score, 1) * (num_classes / 5)
+    i = score2index(score)
     z = np.zeros(num_classes)
     if i != 0:
         z[int(i)] = 1
@@ -29,7 +38,7 @@ def encode_score(score):
 
 def decode_score(score):
     i = np.where(score==1)[0]
-    return (i / (num_classes / 5))
+    return index2score(i)
 
 
 def upsample(x_lr, d, r):
@@ -61,23 +70,24 @@ def resample(x, original_sr, new_sr, filename, noise):
         r[b] = np.random.rand(1) * 0.05
         resampled += r
 
-    filename_out = os.path.join('temp', str(sr) + '_' + str(noise) + '_' + filename)
+    filename_out = os.path.join('temp', str(new_sr) + '_' + str(noise) + '_' + filename)
     sf.write(filename_out, resampled, pesq_sr, 'PCM_16')
     return (filename_out, resampled)
 
 
 h5_file = h5py.File(input_hdf5, 'r')
 hr_dataset = h5_file['hr']
-
 scores = []
 patches = []
+
+print(hr_dataset.shape)
 
 indexes = list(range(hr_dataset.shape[0]))
 random.shuffle(indexes)
 for hi in indexes:
 
     # grab a hr patch from the hdf5
-    x = hr_dataset[hi, 0, :]
+    x = hr_dataset[hi, :]
     hr_filename = '{:d}_hr.wav'.format(hi)
 
     # store filenames in here of the downsampled and noisy (degraded) versions of this patch
@@ -111,12 +121,30 @@ for hi in indexes:
 
     scores += these_scores
 
+
+# a = np.array(hr_dataset)[1:10]
+# print(a.shape)
+
+# patches = []
+# scores = []
+
+# with Pool(8) as p:
+#     res = p.imap_unordered(generate, a)
+#     for x in res:
+#         if len(x[0]) > 0 and len(x[1]) > 0:
+#             for i in range(len(x[0][0])):
+#                 patches.append(x[0][i])
+#                 scores.append(x[])
+
 patches = np.expand_dims(np.array(patches, dtype=np.float32), axis=1)
 scores = np.array(scores, dtype=np.int32)
 
+print(patches.shape)
+print(scores.shape)
+
 with h5py.File(output_hdf5, 'w') as f:
     x = f.create_dataset("x", patches.shape, dtype=np.float32)
-    y = f.create_dataset("y", scores.shape, dtype=np.float32)
+    y = f.create_dataset("y", scores.shape, dtype=np.int32)
 
     x[...] = patches
     y[...] = scores
