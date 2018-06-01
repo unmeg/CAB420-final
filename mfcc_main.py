264ce2042286be95f3c28b1ff6f5d9232b3e9596
Meg White <dearmegwhite@gmail.com>
2018-06-01 20:25:41 +1000

    Update mfcc_main.py
    beep boop deleted some variables that i don't think do anything, my bad if it breaks

--- mfcc_main.py
+++ mfcc_main.py
@@ -1,6 +1,4 @@
-# batch norm paper: https://arxiv.org/abs/1502.03167
-# example: https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/models/fcn16s.py
-# block setup borrowed from: https://github.com/eladhoffer/convNet.pytorch/blob/master/models/mnist.py
+# The block set-up below is modeled on this PyTorch example: https://github.com/eladhoffer/convNet.pytorch/blob/master/models/mnist.py
 
 import torch
 import torch.nn as nn
@@ -22,29 +20,29 @@ from testies import Testies
 import numpy as np
 from data.dataloader import HDF5PatchesDataset
 
+
+# Parameters
 checkpoint_label = 'mfcc'
 filter_size = 3
 learning_rate = 1e-4
 starting_epoch = 0
 num_epochs = 50
-num_classes = 50
-training = 1
-
-input_size = 678
+num_classes = 50 # this gives us a category for every step of 0.1 in the PESQ score (0-5)
+training = 1 # Boolean setting for training mode
 
 class AudioMagicNet(nn.Module):
     def __init__(self, blocks):
         super(AudioMagicNet, self).__init__()
 
-        self.features = nn.Sequential()
+        self.features = nn.Sequential() # a module that acts quite like a list/array
 
+        # Starting values
         conv_input = 1
-        output = 16 # get the size
-        fc_in = input_size//output # compute fc size pls
-
+        output = 16 
+        
         for b in range(0,blocks):
             i = b+1
-            self.features.add_module("conv"+str(i),nn.Conv2d(conv_input, output, filter_size, stride=1, padding=1)), # padding/stride?
+            self.features.add_module("conv"+str(i),nn.Conv2d(conv_input, output, filter_size, stride=1, padding=1)), # padding maintains size
             self.features.add_module("bn"+str(i),nn.BatchNorm2d(output)),
             self.features.add_module("relu"+str(i),nn.LeakyReLU()),
             self.features.add_module("pool"+str(i),nn.MaxPool2d(2))
@@ -52,16 +50,16 @@ class AudioMagicNet(nn.Module):
             output = conv_input * 2
 
         print(self.features)
-        self.final = nn.Linear(1920, num_classes) # hardcoded based on known size (h.shape) >>> 128 x 5 x 42
+        self.final = nn.Linear(1920, num_classes) # after features block we have a tensor of [1, 1920]
 
     def forward(self, x):
         h = self.features(x)
         # print('h shape: ', h.shape)
-        h = h.view(h.size(0), -1)
+        h = h.view(h.size(0), -1)  # reshapes tensor into [1, x]
         h = self.final(h)
         return h
 
-net = AudioMagicNet(4)
+net = AudioMagicNet(4) # Instantiates the network with 4 layers
 
 dataset = HDF5PatchesDataset('../dataset/train_pesq_small.hdf5')
 
